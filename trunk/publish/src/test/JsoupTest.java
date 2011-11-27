90a699443b3438eb19cc1b34182af8cedde7d900
jishijun204@163.com <jishijun204@163.com@f27e7870-95f8-11de-adcb-5b537d459081>
2011-11-27 00:38:22 +0000

    sss

--- trunk/publish/src/test/JsoupTest.java
+++ trunk/publish/src/test/JsoupTest.java
@@ -1,11 +1,12 @@
 package test;
 
+import java.io.File;
+import java.io.FileWriter;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-import org.springframework.web.util.HtmlUtils;
+import org.jsoup.nodes.Element;
 
-import com.sily.publish.WordPressPost;
 import com.sily.util.ReadFile;
 
 public class JsoupTest { 
@@ -13,7 +14,7 @@ public class JsoupTest {
 	public static void main(String[] args) {
 		ReadFile rFile = new ReadFile();
 		
-		String content = rFile.getFileContent("d:/我的桌面/simple.html","gb2312");
+		String content = rFile.getFileContent("C:/Users/sily/Desktop/dropbox.html");
 		Document doc = null;
 //		try {
 //			doc = Jsoup.parse(new File("C:/Users/sily/Desktop/text.html"),"utf-8");
@@ -27,16 +28,16 @@ public class JsoupTest {
 		
 //		System.out.println(text);
 		
-		Elements ediv = doc.getElementsByTag("font");
-		Elements br = doc.select("br");
-	
-		
-		ediv.unwrap();
-		
-		String htmlString = doc.body().html();
-		htmlString = HtmlUtils.htmlUnescape(htmlString);
-		htmlString = htmlString.replaceAll("<br />", "");
-		System.out.println(htmlString);
+//		Elements ediv = doc.getElementsByTag("font");
+//		Elements br = doc.select("br");
+//	
+//		
+//		ediv.unwrap();
+//		
+//		String htmlString = doc.body().html();
+//		htmlString = HtmlUtils.htmlUnescape(htmlString);
+//		htmlString = htmlString.replaceAll("<br />", "");
+//		System.out.println(htmlString);
 		
 //	
 //		Iterator li = ediv.iterator();
@@ -47,6 +48,15 @@ public class JsoupTest {
 //		}
 		try {
 //			WordPressPost.publishPost("使用xml-rpc发布wordpress日志",htmlString);
+			
+			Element element = doc.select("div[class=ennote]").first();
+			String html = element.html();
+			FileWriter fWriter = new FileWriter(new File("C:/Users/sily/Desktop/dropbox1.html"));
+			
+			fWriter.write(html);
+			
+			fWriter.flush();
+			fWriter.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
